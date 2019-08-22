4bebcabb58a854fe8f9962809a757cd9bcef05c8
Tommy109 <tokutomi@degino.com>
2019-08-22 17:40:44 +0900

    style: テストコードのスタイルを整えた。

--- src/l2_molecules/forms/text_with_label.spec.js
+++ src/l2_molecules/forms/text_with_label.spec.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import {TextWithLabelPure} from './text_with_label'
 
-describe('', () => {
+describe('タイトル付きテキスト入力について', () => {
   const wrapper = shallow(
     <TextWithLabelPure
       id      = "dummy"
@@ -10,17 +10,22 @@ describe('', () => {
       label   = "ラベル"
     />
   )
+
   describe('label について', () => {
     const target = wrapper.find('label')
+
     it('パラメーターが渡っていること', () => {
       expect(target).toHaveProp('htmlFor', 'dummy')
     })
+
     it('ラベルが渡っていること', () => {
       expect(target).toHaveText('ラベル')
     })
   })
+
   describe('input について', () => {
     const target = wrapper.find('input')
+
     it('パラメーターが渡っていること', () => {
       expect(target).toHaveProp({
         id:     'dummy',
