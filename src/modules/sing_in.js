ec14649d8ec6d342ea1e704fd1222bfa9887a156
Tommy109 <tokutomi@degino.com>
2019-09-01 12:47:12 +0900

    feat: モジュールを追加した。

---
+++ src/modules/sing_in.js
@@ -0,0 +1,29 @@
+// ActionType
+export const ACTION = {
+  UPDATE_TEXT: 'signin/update/text'    // テキスト項目を更新
+}
+
+const initialState = {
+  payload: {
+    account:    '',
+    password:   ''
+  }
+}
+
+// reducer
+const REDUCER = {
+  [ACTION.UPDATE_TEXT]: (state, action) =>
+    ({...state, payload: {...state.payload, [action.name]: action.value}})
+}
+
+export default function reducer(state=initialState, action={type:null}) {
+  if (!(action.type in REDUCER)) return state
+  return REDUCER[action.type](state, action)
+}
+
+//
+// action creator
+//
+
+// テキスト入力
+export const updateText = evt => ({type:ACTION.UPDATE_TEXT, name:evt.target.name, value:evt.target.value})
