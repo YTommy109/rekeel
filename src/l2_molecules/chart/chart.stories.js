47a001072ee74dd722a2ee6edfdb249a5e5627e8
Tommy109 <tokutomi@degino.com>
2019-10-30 21:09:14 +0900

    feat: ラインチャートを追加した。

--- src/l2_molecules/chart/chart.stories.js
+++ src/l2_molecules/chart/chart.stories.js
@@ -3,6 +3,7 @@ import {storiesOf} from '@storybook/react'
 import {action} from '@storybook/addon-actions'
 import Frame from '~/l1_atoms/frame'
 import BarChart from './bar_chart'
+import LineChart from './line_chart'
 
 const datum = [{
   key: 'Cumulative Return',
@@ -42,12 +43,42 @@ const datum = [{
   ]
 }]
 
+const data = [
+  {
+    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  },
+]
+
 storiesOf('l2_molecules', module)
   .add('チャート部品', () =>
-    <Frame>
-      <h2>バーチャートのサンプル</h2>
-      <BarChart datum={datum} />
-    </Frame>
+    <div>
+      <Frame>
+        <h2>バーチャートのサンプル</h2>
+        <BarChart datum={datum} />
+      </Frame>
+      <Frame>
+        <h2>ラインチャートのサンプル</h2>
+        <LineChart data={data} />
+      </Frame>
+    </div>
   , {
     info: {
       inline: true,
