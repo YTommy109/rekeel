6efc0b3b05e597e3b027cff145ae6ddc485b5b32
Tommy109 <tokutomi@degino.com>
2020-03-13 13:32:20 +0900

    test: testcafe のテストを追加した。

--- __cafe__/sign_in.spec.js
+++ __cafe__/sign_in.spec.js
@@ -10,3 +10,15 @@ test('入力エリアがあること', async t => {
     .expect(Selector('#account').exists).ok()
     .expect(Selector('#password').exists).ok()
 })
+
+test('アカウントを入力できること', async t => {
+  await t
+    .typeText(Selector('#account'), 'hoge')
+    .expect(Selector('#account').value).eql('hoge')
+})
+
+test('パスワードを入力できること', async t => {
+  await t
+    .typeText(Selector('#password'), '999')
+    .expect(Selector('#password').value).eql('999')
+})
