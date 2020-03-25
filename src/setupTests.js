57b09b107c5811a0aae776630bbfc9f11643055e
Tommy109 <tokutomi@degino.com>
2020-03-25 18:20:10 +0900

    chore: パッケージを追加した。

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
+import {enableFetchMocks} from 'jest-fetch-mock'
+import 'jest-localstorage-mock'
 
 Enzyme.configure({adapter: new Adapter()})
+enableFetchMocks()
