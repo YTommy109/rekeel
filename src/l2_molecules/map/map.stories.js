c86eb0b5a1072731dbb8808e8bfb71078c2508f7
Tommy109 <tokutomi@degino.com>
2020-03-27 17:07:30 +0900

    style: console.log を消した。

--- src/l2_molecules/map/map.stories.js
+++ src/l2_molecules/map/map.stories.js
@@ -4,23 +4,19 @@ import {action} from '@storybook/addon-actions'
 import CurrentMap from './current_map'
 
 storiesOf('l2_molecules', module)
-  .add('マップ部品', () => {
-    console.log(process.env)
-    return (
-      <div>
-        <CurrentMap style={{height: 180 + 'px'}}
-          handleCurrent   = {action('現在地ボタンを押しました!')}
-          mapClickHandle  = {action('マップをクリックしました!')}
-          center = {[35.651687, 139.789177]}
-          points = {[
-            [35.655640, 139.790989],
-            [35.652031, 139.793092],
-            [35.649076, 139.790860]
-          ]}
-        />
-      </div>
-    )
-  }
+  .add('マップ部品', () =>
+    <div>
+      <CurrentMap style={{width: '480px', height: '360px'}}
+        handleCurrent   = {action('現在地ボタンを押しました!')}
+        mapClickHandle  = {action('マップをクリックしました!')}
+        center = {[35.651687, 139.789177]}
+        points = {[
+          [35.655640, 139.790989],
+          [35.652031, 139.793092],
+          [35.649076, 139.790860]
+        ]}
+      />
+    </div>
   , {
     info: {
       inline: true,
