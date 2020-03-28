c266bfd5c7b9f1f645a9d05894619ae620285aee
Tommy109 <tokutomi@degino.com>
2020-03-28 11:59:27 +0900

    test: react-testing-library 版に書き換えた。

--- src/l1_atoms/button.spec.js
+++ src/l1_atoms/button.spec.js
@@ -1,28 +1,33 @@
 import React from 'react'
-import {shallow} from 'enzyme'
-import {ButtonPure} from './button'
-
-const dummy = () => {}
+import '@testing-library/jest-dom'
+import {render, fireEvent, screen} from '@testing-library/react'
+import Button from './button'
 
 describe('ボタンについて', () => {
-  const wrapper = shallow(
-    <ButtonPure
-      id          = "dummy"
-      label       = "ラベル"
-      handleClick = {dummy}
-    />
-  )
-  const target = wrapper.find('input[type="button"]')
-
+  const handle = jest.fn()
+  beforeEach(() => {
+    render(<Button
+      id          = 'test001'
+      label       = 'BUTTON'
+      handleClick = {handle}
+    />)
+  })
   it('ボタンが存在すること', () => {
-    expect(target).toHaveLength(1)
+    const target = screen.getByText('BUTTON')
+    expect(target).toBeInTheDocument()
   })
-
-  it('パラメーターが渡っていること', () => {
-    expect(target).toHaveProp({
-      id:       'dummy',
-      value:    'ラベル',
-      onClick:  dummy
-    })
+  it('タイプが button であること', () => {
+    const target = screen.getByText('BUTTON')
+    expect(target).toHaveAttribute('type', 'button')
   })
+  it('ID が設定されていること', () => {
+    const target = screen.getByText('BUTTON')
+    expect(target).toHaveAttribute('id', 'test001')
+  })
+  it('クリックで関数が呼ばれること', () => {
+    const target = screen.getByText('BUTTON')
+    fireEvent.click(target)
+    expect(handle).toBeCalledTimes(1)
+  })
+  it.todo('enable/disable を切り替えられること')
 })
