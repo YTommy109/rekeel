88c0e64c17d1536a59cc430ef296db0e97bc1937
Tommy109 <tokutomi@degino.com>
2020-03-28 20:24:42 +0900

    refactor: ツーボタンの pure 方式を廃止した。

--- src/l2_molecules/toolbar/two_button.js
+++ src/l2_molecules/toolbar/two_button.js
@@ -3,33 +3,35 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Button from '~/l1_atoms/button'
 
-export const TwoButtonPure = ({className, config, ...props}) =>
-  <div className={className}>
-    <Button
-      id        = {config[0].id}
-      name      = {config[0].name}
-      label     = {config[0].label}
-      btnStyle  = "normal"
-    />
-    <Button
-      id        = {config[1].id}
-      name      = {config[1].name}
-      label     = {config[1].label}
-      btnStyle  = "primary"
-    />
-  </div>
-
-TwoButtonPure.propTypes = {
-  className:  PropTypes.string,
-  config:     PropTypes.arrayOf(PropTypes.shape({
-    id:       PropTypes.string.isRequired,
-    name:     PropTypes.string.isRequired,
-    label:    PropTypes.string.isRequired
-  }))
-}
-
-const TwoButton = styled(TwoButtonPure)`
+const Div = styled.div`
     text-align: right;
 `
+
+const TwoButton = ({config, ...props}) =>
+  <Div {...props}>
+    <Button
+      id          = {config[0].id}
+      name        = {config[0].name}
+      label       = {config[0].label}
+      btnStyle    = "normal"
+      handleClick = {config[0].handleClick}
+    />
+    <Button
+      id          = {config[1].id}
+      name        = {config[1].name}
+      label       = {config[1].label}
+      btnStyle    = "primary"
+      handleClick = {config[1].handleClick}
+    />
+  </Div>
+
+TwoButton.propTypes = {
+  config:         PropTypes.arrayOf(PropTypes.shape({
+    id:           PropTypes.string.isRequired,
+    name:         PropTypes.string.isRequired,
+    label:        PropTypes.string.isRequired,
+    handleClick:  PropTypes.func.isRequired
+  }))
+}
 
 export default TwoButton
---
+++ src/l2_molecules/toolbar/two_button.spec.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import {render, fireEvent, screen} from '@testing-library/react'
+import TwoButton from './two_button'
+
+describe('ツーボタンバーについて', () => {
+  const btn1 = jest.fn()
+  const btn2 = jest.fn()
+  const config = [{
+    id:           'id1',
+    name:         'name1',
+    label:        'label1',
+    handleClick:  btn1
+  }, {
+    id:           'id2',
+    name:         'name2',
+    label:        'label2',
+    handleClick:  btn2
+  }]
+  beforeEach(() => {
+    btn1.mockClear()
+    btn2.mockClear()
+    render(<TwoButton config={config}/>)
+
+  })
+  describe('ボタン1 について', () => {
+    it('存在すること', () => {
+      const target = screen.getByDisplayValue('label1')
+      expect(target).toBeInTheDocument()
+    })
+
+    it('プロパティが渡されていること', () => {
+      const target = screen.getByDisplayValue('label1')
+      expect(target).toHaveAttribute('id', 'id1')
+      expect(target).toHaveAttribute('name', 'name1')
+    })
+
+    it('ハンドラーが呼ばれること', () => {
+      const target = screen.getByDisplayValue('label1')
+      fireEvent.click(target, {target: {}})
+      expect(btn1).toBeCalledTimes(1)
+    })
+  })
+
+  describe('ボタン2 について', () => {
+    it('ボタン2 が存在すること', () => {
+      const target = screen.getByDisplayValue('label2')
+      expect(target).toBeInTheDocument()
+    })
+
+    it('プロパティが渡されていること', () => {
+      const target = screen.getByDisplayValue('label2')
+      expect(target).toHaveAttribute('id', 'id2')
+      expect(target).toHaveAttribute('name', 'name2')
+    })
+
+    it('ハンドラーが呼ばれること', () => {
+      const target = screen.getByDisplayValue('label2')
+      fireEvent.click(target, {target: {}})
+      expect(btn2).toBeCalledTimes(1)
+    })
+  })
+})
