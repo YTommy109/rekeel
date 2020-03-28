8a9418b687c597527e63a44d2aebe8d02358bcc6
Tommy109 <tokutomi@degino.com>
2020-03-28 13:15:13 +0900

    refactor: フレームを pure なしにした。

--- src/l1_atoms/frame.js
+++ src/l1_atoms/frame.js
@@ -2,21 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-export const FramePure = ({className, ...props}) =>
-  <div className={className}>
-    {props.children}
-  </div>
-
-FramePure.propTypes = {
-  className:    PropTypes.string,
-  children:     PropTypes.node
-}
-  
-const Frame = styled(FramePure)`
+const FrameBase = styled.div`
   width:          ${props => props.width || '640px'};
   border:         solid thin gainsboro;
   border-radius:  10px;
   padding:        8px;
 `
 
+const Frame = props =>
+  <FrameBase {...props} />
+
+Frame.propTypes = {
+  children:     PropTypes.node
+}
+  
 export default Frame
