74fab11feffea97b34422f5b91fde62f360c82b3
Tommy109 <tokutomi@degino.com>
2020-03-29 22:44:19 +0900

    chore: テストコードを消した。

--- src/l2_molecules/map/current_map.spec.js
+++ src/l2_molecules/map/current_map.spec.js
@@ -1,52 +1,29 @@
 import React from 'react'
-import {shallow} from 'enzyme'
-import {Map, TileLayer, Polyline} from 'react-leaflet'
-import {CurrentMapPure} from './current_map'
+import '@testing-library/jest-dom'
+import {render, fireEvent, screen} from '@testing-library/react'
+import CurrentMap from './current_map'
+jest.mock('leaflet')
 
 describe('現在地マップについて', () => {
-  const point = [35.651687, 139.789177]
+  const center = [35.651687, 139.789177]
   const points = [
-    new L.LatLng(35.655640, 139.790989),
-    new L.LatLng(35.652031, 139.793092),
-    new L.LatLng(35.649076, 139.790860)
+    [35.655640, 139.790989],
+    [35.652031, 139.793092],
+    [35.649076, 139.790860]
   ]
-  const handle = () => {}
-  
-  const wrapper = shallow(
-    <CurrentMapPure
-      center          = {point}
-      points          = {points}
-      mapClickHandle  = {handle}
-    />
-  )
-
-  it('Map があること', () => {
-    const target = wrapper.find(Map)
-
-    expect(target).toHaveProp({
-      center:   point,
-      onClick:  handle
-    })
+  const handle = jest.fn()
+  beforeEach(() => {
+    handle.mockClear()
+    render(
+      <CurrentMap
+        center = {center}
+        points = {points}
+      />
+    )
   })
 
-  it('TileLayer があること', () => {
-    const target = wrapper.find(TileLayer)
-
-    expect(target).toHaveLength(1)
-  })
-
-  it('Polyline があること', () => {
-    const target = wrapper.find(Polyline)
-
-    expect(target).toHaveLength(1)
-    expect(target).toHaveProp({
-      positions: points
-    })
-  })
-
-  it('Marker があること', () => {
-    const target = wrapper.find('[data-testid="point"]')
-
-    expect(target).toHaveLength(points.length)
-  })
+  it.todo('Map があること')
+  it.todo('TileLayer があること')
+  it.todo('Polyline があること')
+  it.todo('points と同じ数の Marker があること')
 })
