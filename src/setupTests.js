6f32ef42be65cd712b8f8ba4870805b5d111c6cf
Tommy109 <tokutomi@degino.com>
2020-03-29 22:47:19 +0900

    chore: enzyme を取り除いた。

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,4 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
 import {enableFetchMocks} from 'jest-fetch-mock'
 import 'jest-localstorage-mock'
 
-Enzyme.configure({adapter: new Adapter()})
 enableFetchMocks()
